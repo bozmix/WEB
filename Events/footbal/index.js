785def7bc721521cad1b2170bc9bb4b9bf23f48a
bozmix <mijailovic.bozidar@gmail.com>
2021-06-23 16:25:12 +0200

    events exercise

--- Events/footbal/index.js
+++ Events/footbal/index.js
@@ -1,11 +1,24 @@
 var player = document.getElementById('player');
 var field = document.getElementById('field');
-
+var control = document.getElementById('control');
 
 function move (click){
-    player.style.top = click.pageY + 'px';
-    player.style.left = click.pageX + 'px';
+    if (field.className === 'active'){
+        player.style.top = click.pageY + 'px';
+        player.style.left = click.pageX + 'px';
+        
+    }
 }
 
+function stop(){
+    field.classList.toggle('active');
+    if (field.className === 'active'){
+        control.value = 'stop moving';
+    } else {
+        control.value = 'start moving';
+    }
+}
 
 field.addEventListener('click', move);
+control.addEventListener('click', stop);
+
