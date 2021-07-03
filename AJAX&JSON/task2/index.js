fabf23ebeebdbe9e92f9f4f29b1639ee13213f9e
bozmix <mijailovic.bozidar@gmail.com>
2021-07-03 13:18:45 +0200

    github searcher

--- AJAX&JSON/task2/index.js
+++ AJAX&JSON/task2/index.js
@@ -1,25 +1,25 @@
-var button = document.getElementById('loadNew');
-var body = document.querySelector('body');
-var img1 = document.getElementsByTagName('img');
+var button = document.getElementById("loadNew");
+var body = document.querySelector("body");
+var img1 = document.getElementsByTagName("img");
 
-function addImage (){
-var request = new XMLHttpRequest();
+function addImage() {
+  var request = new XMLHttpRequest();
 
-request.open('GET', 'https://dog.ceo/api/breeds/image/random', true);
+  request.open("GET", "https://dog.ceo/api/breeds/image/random", true);
 
-request.onload = function(){
-    if (request.status >= 200 && request.status < 400){
-        var data = JSON.parse(request.responseText);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      var data = JSON.parse(request.responseText);
     }
-    if (img1[0]){
-        body.removeChild(img1[0]);
+    if (img1[0]) {
+      body.removeChild(img1[0]);
     }
 
-    var img = document.createElement('img');
-    img.setAttribute('src', data.message);
+    var img = document.createElement("img");
+    img.setAttribute("src", data.message);
     body.appendChild(img);
-}
-request.send();
+  };
+  request.send();
 }
 
-button.addEventListener('click', addImage);
+button.addEventListener("click", addImage);
