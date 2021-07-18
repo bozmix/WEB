08a7e09f27324b476af577e37d7f85a0d2bf297a
bozmix <mijailovic.bozidar@gmail.com>
2021-07-18 10:13:51 +0200

    AJAX tasks

--- AJAX&JSON/task3/index.js
+++ AJAX&JSON/task3/index.js
@@ -1,8 +1,8 @@
 var input = document.getElementById('search');
 var resultsDisplay = document.getElementById('results');
 var api = 'https://api.github.com/search/users?q=';
-
-someFunction = function(event){
+ 
+export const someFunction =(event) =>{
     if (event.keyCode === 13){
         var inputValue = input.value;
         $.ajax(api + inputValue, {
